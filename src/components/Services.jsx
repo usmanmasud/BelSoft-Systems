698ad1f54417120e9e6ae66b79e2c75c4d01f630
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2024-09-25 23:25:19 +0100

    ///

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -12,14 +12,14 @@ const ServicesSection = () => {
         <section className="py-16">
             <div className="container mx-auto px-6">
                 <h2 className="text-3xl font-bold text-left  p-[30px] text-[rgba(0,181,116,1)]">My Services</h2>
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-90">
+                <div className="grid grid-cols-1 md:grid-cols-2  gap-90">
                     {services.map((service, index) => (
                         <div key={index} className="flex m-10 items-centerp-4">
                             <img src={service.image} alt={service.title} className="w-20 h-20 mr-4" />
 
                             <div>
                                 <h3 className="text-xl font-semibold text-gray-800">{service.title}</h3>
-                                <p className="text-gray-600 w-[350px]">{service.description}</p>
+                                <p className="text-gray-600">{service.description}</p>
                             </div>
                         </div>
                     ))}
