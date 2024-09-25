0c02cf8d66ad80a6031aa75fd4a09e62d305415a
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2024-09-26 00:37:27 +0100

    res....

--- src/components/IntoSection.jsx
+++ src/components/IntoSection.jsx
@@ -3,16 +3,16 @@ import React from 'react';
 const IntroSection = () => {
     return (
         <section className="py-16">
-            <div className="container mx-auto  md:flex md:items-center sm:p-8">
+            <div className="container mx-auto  md:flex md:items-center">
                 {/* Text Section */}
-                <div className="md:w-1/2 p-0 m-0">
-                    <div className='w-[592px] h-[291px]'>
+                <div className="md:w-1/2 lg:p-0 lg:m-0 md:p-0 md:m-0">
+                    <div className='lg:w-[592px] md:w-[592px] md:h-[291px] lg:h-[291px]'>
                         <h2 className="md:text-[100px] lg:text-[120px] text-[70px] font-bold text-[#000000] mb-[-55px]">Hey there,
                         </h2>
                         <h2 className='md:text-[100px] lg:text-[120px] text-[70px] leading-[150px] font-bold text-[#00B574]'>I'm Terry</h2>
                     </div>
 
-                    <p className="text-lg text-[#474747] sm:pl-20  mb-2 lg:w-[454px] h-[112px]">
+                    <p className="text-lg text-[#474747]  mb-2 lg:w-[454px] lg:h-[112px] md:h-[112px]">
                         I craft intuitive designs that connect with users and drive results. As a multidisciplinary designer, I bring ideas to life through a perfect blend of creativity, strategy, and user-centered design.
                         Lets talk
                     </p>
@@ -26,7 +26,7 @@ const IntroSection = () => {
                     <img
                         src="/person.png"
                         alt="image"
-                        className="lg:w-[9000px] md:w-[9000px] lg:h-[600px] md:h-[600px] sm:rounded-lg"
+                        className="lg:w-[9000px] w-[100%] md:w-[9000px] lg:h-[600px] md:h-[600px] sm:rounded-lg"
                     />
                 </div>
             </div>
